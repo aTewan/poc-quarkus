e8d4c7ba5235e2978b1bfef916d4cce56f43da0b
aTewan <tewan33@gmail.com>
2020-10-07 21:22:00 +0200

    Ajout d'un microservice de récupération des users

--- users-read-service/src/main/java/org/antoinep/users/UsersReadOnlyResource.java
+++ users-read-service/src/main/java/org/antoinep/users/UsersReadOnlyResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("/users")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class UsersReadOnlyResource {
 
--- users-read-service/src/test/java/org/antoinep/users/UsersReadOnlyResourceTest.java
+++ users-read-service/src/test/java/org/antoinep/users/UsersReadOnlyResourceTest.java
@@ -1,21 +1,8 @@
 package org.antoinep.users;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class UsersReadOnlyResourceTest {
-
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/users")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
 
 }
